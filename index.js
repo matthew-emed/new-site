f33463c04037da15aa1d1d67e2192a2001232a3c
Matthew Angel <matthewangel@Loaners-MacBook-Pro.local>
2021-10-26 11:00:58 -0400

    changed video to still imahe

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-console.log("hello")
-
-
-var app = document.querySelector('.header');
+var app = document.getElementById('header-text');
 
 var typewriter = new Typewriter(app, {
-    loop: true
+  loop: true,
+  delay: 35,
 });
 
-typewriter.typeString('Hello World!')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('My Name is Matthew Angel')
-    .pauseFor(2500)
-    .deleteAll()
-    .start();
+typewriter
+  
+  .typeString('Greetings All')
+ .pauseFor(1000)
+.deleteChars(13) 
+  .typeString('My Name is Matthew Angel')
+  .pauseFor(1000)
+.deleteChars(26) 
+  .pauseFor(1000)
+  .typeString('Welcome to my page!')
+  .pauseFor(1500)
+  .deleteChars(26) 
+  .start();
